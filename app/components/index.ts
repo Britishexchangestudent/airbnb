3cc7d7cc7b0d111d10f8c9efee0c315bfbea708a
Dan Ahmed <44755600+Britishexchangestudent@users.noreply.github.com>
2023-05-27 16:34:35 +0100

    added nav

---
+++ app/components/index.ts
@@ -0,0 +1,4 @@
+export {default as Navbar} from './Navbar/Navbar';
+export {default as Logo} from './Navbar/Logo';
+export {default as Container} from './Container';
+export {default as Avatar} from './Avatar';
